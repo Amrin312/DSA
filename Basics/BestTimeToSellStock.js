bbfc60700950ba545eb000697f635ee89d702063
Amrin <amrinfathimaaf321@gmail.com>
2026-07-29 12:31:07 +0530

    DSA Day 7- qns updated

--- Basics/BestTimeToSellStock.js
+++ Basics/BestTimeToSellStock.js
@@ -1,4 +1,4 @@
-let arr = [7,1,5,3,6,4];
+const arr = [7,1,5,3,6,4];
 
 function findStock(arr){
     let min = arr[0];
@@ -19,3 +19,18 @@ function findStock(arr){
 console.log(findStock(arr));
 
 // store the min buy day and find the max profit from selling day and return maxProfit
+
+
+// DRY RUN
+// 1 = 1-7 = -6 > 0 , min = 1 < 7 = 1
+// 5 = 5-1 = 4 > 0 => 4, min = 5 < 1
+// 3 = 3-1 = 2 > 4 , min = 3<1
+// 6 = 6-1 =5 > 4 => 5, min = 5<1
+// 4 = 4-1 = 3 >5, min 4<1;
+
+// Leetcode 121
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
