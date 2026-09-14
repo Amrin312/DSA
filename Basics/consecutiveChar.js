58b54675135706a2b33e2b3cc9ae4038fdc979b3
Amrin <amrinfathimaaf321@gmail.com>
2026-09-14 19:56:46 +0530

    day 15

---
+++ Basics/consecutiveChar.js
@@ -0,0 +1,20 @@
+const str ="aaabbcddd";
+
+function char(str){
+    let result = '';
+    let count = 1;
+
+    for(let i=1; i<=str.length; i++){
+
+        if(str[i] === str[i-1]){
+            count++;
+        }else{
+            result += str[i-1] + count;
+            count = 1;
+        }
+    }
+
+    return result
+}
+
+console.log(char(str));
